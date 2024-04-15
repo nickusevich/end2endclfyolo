694f6b5c662abb687dc3202239c79c6bff0d6d66
nickusevich <nickusevich1@gmail.com>
2024-04-15 16:44:15 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'end2endclfyolo',
+    version= '0.0.0',
+    author= 'Nikita Aksjonov',
+    author_email= 'nickusevich1@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
